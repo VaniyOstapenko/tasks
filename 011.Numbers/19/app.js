1e9c9c67a8bee2f6c90d1fb8a2fc5d34f8249112
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-06-29 11:38:46 +0300

    added tasks

--- 011.Numbers/19/app.js
+++ 011.Numbers/19/app.js
@@ -3,9 +3,15 @@
 //  ax2 + bx + c = 0 
 //  Если уравнение имеет два корня, то следует вывести их в порядке возрастания.
 
-let a, b, c = +prompt('ввидети значения');
+let a = +prompt('введите значение');
+let b = +prompt('введите значение');
+let c = +prompt('введите значение');
 
-if (a) {
-    console.log(D = b ** 2 - 4 * a * c);
+if (a + b + c === 0) {
+    console.log(x1 = 1, x2 = c / a);
+} else if (b ** 2 - 4 * a * c < 0) {
+    console.log('нет действительных корней');
+} else if (b ** 2 - 4 * a * c > 0) {
+    console.log(-b + Math.sqrt(b ** 2 - 4 * a * c) / 2 * a);
+    console.log(-b - Math.sqrt(b ** 2 - 4 * a * c) / 2 * a);
 }
-
