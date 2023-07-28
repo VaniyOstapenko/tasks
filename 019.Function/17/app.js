7443c9089f750c54245ed59dc6fa01f0d9e99f59
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-07-28 20:19:55 +0300

    added tasks

--- 019.Function/17/app.js
+++ 019.Function/17/app.js
@@ -2,5 +2,19 @@
 // Разбить строку на массив. Функция должна вернуть строку,
 // где каждый элемент массива в чередование регистров toLowerCase, toUpperCase hschool-> HsChOoL
 
-const str = "hschool";
+const str = "hschool".split("");
 
+function doUppLow(str) {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0) {
+      arr.push(str[i].toUpperCase());
+    } else {
+      arr.push(str[i].toLowerCase());
+    }
+  }
+  return arr.join("");
+}
+
+const result = doUppLow(str);
+console.log(result);
