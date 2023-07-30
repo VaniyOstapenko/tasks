055bd6dee69cd1a0ffe58f441da827eaef351738
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-07-30 13:56:43 +0300

    added tasks

--- 020.Context/10/app.js
+++ 020.Context/10/app.js
@@ -1,2 +1,18 @@
-// Напишите функцию, которая принимает два параметра: предложение из нескольких слови букву. 
+// Напишите функцию, которая принимает два параметра: предложение из нескольких слов и букву.
 // Функция должна подсчитывать количество вхождений указанной буквы в строке.Добавить проверки
+
+let str = "manchester united".split("");
+let letter = "e";
+
+function doCountLetter(str, letter) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === letter) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let result = doCountLetter(str, letter);
+console.log(result);
--- 020.Context/9/app.js
+++ 020.Context/9/app.js
@@ -1,17 +1,28 @@
 // На входе динамичный массив;число n с клавиатуры. Необходимо написать функцию,
 // возвращающую элементы массива, которые больше указанного числа. [1, 9, 45, 11, 10], 10 -> 45, 11
 
-const n = prompt("количество элементов массива");
+const n = +prompt("количество элементов массива");
 
 function doArray(n) {
   let arr = [];
   for (let i = 0; i < n; i++) {
-    arr.push(prompt("элемент массива"));
+    arr.push(+prompt("элемент массива"));
   }
   return arr;
 }
 
 let result = doArray(n);
 
+const num = +prompt("число по условию");
 
+function doNewArray(num) {
+  let new_arr = result.filter(function (el) {
+    if (el > num) {
+      return true;
+    }
+  });
+  return new_arr
+}
 
+let result2 = doNewArray(num)
+console.log(result2);
