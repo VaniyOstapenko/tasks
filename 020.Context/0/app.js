e80861eb4c689f70b01cff60a737dc64157d3a63
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-08-06 21:04:58 +0300

    added tasks

--- 020.Context/0/app.js
+++ 020.Context/0/app.js
@@ -10,13 +10,27 @@ const obj = {
 }
 obj.test()
 
-if(true){
+if (true) {
     console.log(this);//window
 }
 
-function test(){
+function test() {
     console.log(this);//window
 }
 test()
 
+//Сортировка пузырьком
 
+function bubble(arr) {
+    for (let j = arr.length - 1; j > 0; j--) {
+        for (let i = 0; i < j; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+    console.log(arr);
+}
+bubble([1, 5, 4, 3]);
