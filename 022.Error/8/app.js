9eb6365fb16aacd2d50b942ea35a90da57c61878
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-08-12 19:18:53 +0300

    added tasks

--- 022.Error/8/app.js
+++ 022.Error/8/app.js
@@ -2,3 +2,25 @@
 // букв и возвращает отсутствующую в массиве. Добавить проверки 
 // ["a","b","c","d","f"] -> "e" ["O","Q","R","S"] -> "P"
 
+const arr = ["a", "b", "c", "d", "f"].join('').toLowerCase();
+
+
+
+function searchLetter(arr) {
+    try {
+        const str = 'abcdefghijklmnopqrstuvwxyz';
+        const indexStr = str.indexOf(arr[0]);
+        const sliceStr = str.slice(indexStr, indexStr + arr.length + 1);
+
+        for (let i = 0; i < sliceStr.length; i++) {
+            if (sliceStr[i] !== arr[i]) {
+                console.log(sliceStr[i]);
+                break;
+            }
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+searchLetter(arr);
