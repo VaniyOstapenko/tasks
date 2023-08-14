b6320d30779ff63ed5ad3a4ed22c0a6bae9e4717
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-08-14 20:40:22 +0300

    added tasks

--- 023.Regex/10/app.js
+++ 023.Regex/10/app.js
@@ -2,3 +2,36 @@
 // комментариев в ветке development и попросил их убрать. Вырезая куски кода вы поняли, что их чересчур много. 
 // Необходимо написать регулярное выражение, которое вырежет все комментарии из html разметки <!---->
 
+const str = `    <header>
+<div class="logo"></div>
+<nav>
+    <p>
+        <a href="../pages/index.html">Work</a>
+    </p>
+    <p>
+    <!--this is parragraf1-->
+        <a href="../pages/abaut.html">About</a>
+    </p>
+    <p>
+    <!--this is parragraf2-->
+        <a href="https://github.com/VaniyOstapenko">Playground</a>
+    </p>
+    <p>
+    <!--this is parragraf3-->
+        <a href="../pages/contact.html">Contact</a>
+    </p>
+</nav>
+</header>
+`;
+
+function doDelete(str) {
+    try {
+        let newStr = str.replaceAll(/^( )+<!--[\w.\s]+-->$/gm, '');
+        return newStr;
+    } catch (error) {
+        return error;
+    }
+}
+
+const result = doDelete(str);
+console.log(result);
