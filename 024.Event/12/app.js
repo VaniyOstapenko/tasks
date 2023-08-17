9fbb9851efc8ce2565262b5c7ac551b13ffa5f23
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-08-17 22:42:09 +0300

    added tasks

--- 024.Event/12/app.js
+++ 024.Event/12/app.js
@@ -1,11 +1,22 @@
 // Вывести в строчку все вводимые значения через инпут в виде массива. Добавить проверку, что поле не пустое
 
 const btn = document.querySelector('button');
+let arr = [];
 
+function isValid(inp) {
+    if (!inp) {
+        throw new Error('Пустой ввод')
+    }
+}
 btn.addEventListener('click', function () {
-    const inp = document.querySelector('input');
-    let arr = [];
-    for (let i = 0; i < inp.value; i++) {
+    try {
+        const inp = document.querySelector('input');
+        const par = document.querySelector('p')
+        isValid(inp.value);
         arr.push(inp.value);
+        par.innerHTML = arr;
+        inp.value = ''
+    } catch (error) {
+        alert(error.message);
     }
 })
