896d39837e7c9701c34822fc35b50e3172a8eb04
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-09-10 23:00:07 +0300

    added tasks

--- 032.OOP_2/9/app.js
+++ 032.OOP_2/9/app.js
@@ -1,12 +1,18 @@
 // Отобразить в div массив из 15 рандомных значений от 0 до 100. Необходимо распознать по какому из значений 
 // контейнера производится событие клика. Если клик происходит, то удалить данное значение из массива в div
 
-const div = document.querySelector('.result');
 const arr = [];
 
-div.addEventListener('click', function () {
-    for (let i = 0; i < 15; i++) {
-        arr.push(Math.floor(Math.random() * 100));
-    }
-    div.innerHTML = arr;
+const result = document.querySelector('.result')
+
+for (let i = 0; i < 15; i++) {
+    arr.push(Math.floor(Math.random() * 100));
+}
+result.innerHTML = arr;
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function (event) {
+    const new_arr = document.querySelector('.new_arr');
+    new_arr.innerHTML = event.target.textContent;
 })
