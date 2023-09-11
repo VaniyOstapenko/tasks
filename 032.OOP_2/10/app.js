82ed04622851f9f7386260c363624b852575c7c4
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-09-11 22:42:10 +0300

    added tasks

--- 032.OOP_2/10/app.js
+++ 032.OOP_2/10/app.js
@@ -13,11 +13,15 @@ btn.addEventListener('click', function () {
     try {
         const inp = document.querySelector('input');
         const div = document.querySelector('div');
+
         isValid(inp.value)
         arr.push(inp.value);
         div.innerHTML = arr;
+        div.style = 'background-color:white';
         inp.value = '';
     } catch (error) {
+        const div = document.querySelector('div');
+        div.style = 'background-color:red';
         div.innerHTML = error.message;
     }
 })
