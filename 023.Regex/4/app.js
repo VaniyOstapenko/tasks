e90d14b3c3f43c7850bebf59a43db88c136f7be5
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-09-16 19:02:41 +0300

    added tasks

--- 023.Regex/4/app.js
+++ 023.Regex/4/app.js
@@ -3,18 +3,17 @@
 // Если же строка подходит под регулярное выражение, то вывести булевое true, в противном случае 
 // бросить исключение и обработать
 
-const str = 'ostapenko_12-29-94_vaniy@mail.com';
-
-function isValid(str) {
-    try {
-        if (!/^[\w\.\-\$]+@+[a-z]+\.[a-z]{2,4}$/g.test(str)) {
-            throw new Error('глупец писать научись почту');
+class Regex {
+    isValid(n) {
+        try {
+            if (!/^[\w\.\-\$]+@+[a-z]+\.[a-z]{2,4}$/g.test(n)) {
+                throw new Error('глупец писать научись почту');
+            }
+            return true;
+        } catch (error) {
+            return error.message;
         }
-        return true;
-    } catch (error) {
-        return error.message;
     }
 }
-
-const result = isValid(str);
-console.log(result);
+const result = new Regex();
+console.log(result.isValid('ostapenko_12-29-94_vaniy@mail.com'));
