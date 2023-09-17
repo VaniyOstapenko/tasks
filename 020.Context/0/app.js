a50c76420be21c62cea1b37872977984db5ccb89
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-09-17 15:19:28 +0300

    added tasks

--- 020.Context/0/app.js
+++ 020.Context/0/app.js
@@ -1,36 +1,60 @@
-//Тренировочный урок контекст this
+// //Тренировочный урок контекст this
 
-console.log(this);//window
+// console.log(this);//window
 
-const obj = {
-    name: 'Ivan',
-    test: function () {
-        console.log(this.id);//undefined//если ключ который есть в объекте, то выведет объект
-    }
-}
-obj.test()
+// const obj = {
+//     name: 'Ivan',
+//     test: function () {
+//         console.log(this.id);//undefined//если ключ который есть в объекте, то выведет объект
+//     }
+// }
+// obj.test()
 
-if (true) {
-    console.log(this);//window
-}
+// if (true) {
+//     console.log(this);//window
+// }
 
-function test() {
-    console.log(this);//window
-}
-test()
+// function test() {
+//     console.log(this);//window
+// }
+// test()
 
-//Сортировка пузырьком
+// //Сортировка пузырьком
 
-function bubble(arr) {
-    for (let j = arr.length - 1; j > 0; j--) {
-        for (let i = 0; i < j; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-            }
-        }
-    }
-    console.log(arr);
-}
-bubble([1, 5, 4, 3]);
+// function bubble(arr) {
+//     for (let i = arr.length - 1; i > 0; i--) {
+//         for (let j = 0; j < i; j++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let num = arr[i];
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = num;
+//             }
+//         }
+//     }
+//     console.log(arr);
+// }
+
+// bubble([1, 3, 2, 5, 4]);
+
+// Фибоначчи
+
+// const n = 10;
+
+// const arr = [0, 1];
+
+// for (let i = 2; i < n; i++) {
+//     let sum = arr[i - 1] + arr[i - 2];
+//     arr.push(sum)
+// }
+
+// console.log(arr);
+
+// Факториал
+
+// function Recurs(n) {
+//     if (n <= 1) return 1;
+//     return n * Recurs(n - 1);
+// }
+
+// console.log(Recurs(3));
+
