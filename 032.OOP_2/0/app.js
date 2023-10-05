b742f5b04b77fd9f7d8f6088f8696bcce4b9329d
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-10-05 16:43:40 +0300

    added tasks

--- 032.OOP_2/0/app.js
+++ 032.OOP_2/0/app.js
@@ -1,26 +1,23 @@
-// Вывести в строчку все вводимые значения через инпутв виде массива, а также все удвоенные значения данного 
-// массива в div ниже. Добавить проверку, что поле не пустое
+// Необходимо отобразить числовой инпути кнопку. 
+// После нажатия на кнопку необходимо получить значение 
+// из инпута. Проверить, что это число > 0, а также, 
+// добавить проверку на ввод только чисел. Если проверка 
+// true, то вывестив div удвоенное значение инпута
 
 const btn = document.querySelector('button');
-const arr = [];
 function isValid(inp) {
-    if (!inp) {
-        throw new Error('Пусстой ввод')
+    if (isNaN(inp)) {
+        throw new Error('не число');
+    } else if (inp <= 0) {
+        throw new Error('меньше 0');
     }
 }
 btn.addEventListener('click', function () {
+    const inp = document.querySelector('input').value;
+    const div = document.querySelector('div');
     try {
-        const inp = document.querySelector('input');
-        const new_arr = document.querySelector('.arr');
-        const mult = document.querySelector('.mult');
-        isValid(inp.value);
-        arr.push(inp.value);
-        new_arr.innerHTML = arr;
-        inp.value = '';
-        const array = arr.map(function (el) {
-            return el * 2;
-        })
-        mult.innerHTML = array;
+        isValid(inp);
+        div.innerHTML = inp * 2;
     } catch (error) {
         alert(error.message);
     }
