8df8fa2a0d812c5ed0ecc1dafed3440ef1b83764
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-10-16 23:41:57 +0300

    added tasks

--- 035.NODE_START/index.js
+++ 035.NODE_START/index.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-    const arr = [1, 1, 4, 5, 5, 2, 6]
+    const arr = [1, 2, 2, 2, 3, 4, 5, 5, 6, 2];
     const new_arr = []
     for (let i = 0; i < arr.length; i++) {
         if (!new_arr.includes(arr[i])) {
@@ -16,26 +16,27 @@ app.get('/', (request, response) => {
 app.get('/:id', (request, response) => {
     const { id } = request.params;
     const arr = [1, 2, 3, 4, 5, 6];
-    const resArr = [];
-    let itemArr = [];
+    const new_arr = [];
+    let item = [];
     for (let i = 0; i < arr.length; i++) {
-        itemArr.push(arr[i]);
-        if (itemArr.length == id) {
-            resArr.push(itemArr)
-            itemArr = [];
+        item.push(arr[i]);
+        if (item.length == id) {
+            new_arr.push(item);
+            item = []
         }
     }
-    response.send(resArr)
+    item.length === 0 ? null : new_arr.push(item);
+    response.send(new_arr);
 })
 
-app.get('/about', (request, response) => {
-    response.send('Hello this is route about')
+
+app.get('/about', (reques, response) => {
+    response.send('Hello world!')
 })
 
-app.get('/contact', (request, response) => {
-    response.send('Hello this is contact "Vaniy"')
+app.get('/contact', (reques, response) => {
+    response.send('Hello Vaniy!')
 })
-
 app.listen(3000, () => {
-    console.log('Server is running on port:3000');
+    console.log('Server is runnig on port:3000');
 })
