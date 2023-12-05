573b63b1dde81b0eec166cda0284df74406827da
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-12-05 13:17:45 +0300

    added tasks

--- 014.Symbol/20/app.js
+++ 014.Symbol/20/app.js
@@ -2,24 +2,13 @@
 // элементов будущего массива Необходимо составить ряд Фибоначчи,
 // где каждое число образуется при сложении 2 предыдущих
 
-// 10 -> 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+const n = 10;
 
-// let a = 10;
-// let arr = "0,1";
-// let num = 0;
-// let str = 1;
-// for (let i = 3; i <= a; i++) {
-//   let sum = num + str;
-//   num = str;
-//   str = sum;
-//   arr += `, ${sum}`;
-// }
-// console.log(arr);
-
-let a = 10;
 let arr = [0, 1];
-for (let i = 2; i < a; i++) {
-  let sum = arr[i - 1] + arr[i - 2];
+
+for (let i = 2; i < n; i++) {
+  const sum = arr[i - 1] + arr[i - 2];
   arr.push(sum);
 }
+
 console.log(arr);
