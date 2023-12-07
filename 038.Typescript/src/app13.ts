5e458cd61ddf2a0c128ddc3e9957dd34d2e4c61a
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-12-07 22:16:02 +0300

    added tasks

--- 038.Typescript/src/app13.ts
+++ 038.Typescript/src/app13.ts
@@ -1,4 +1,11 @@
 //Создайте переменную sentence и присвойте ей строковое значение предложения. Напишите программу, которая преобразует первую
 //букву каждого слова в предложении в верхний регистр. Например, для предложения "hello world" результатом должна быть строка "Hello World".
 
+const sentence: string[] = "hello world".split(" ");
 
+let arr: string[] = [];
+for (let i = 0; i < sentence.length; i++) {
+  arr.push(sentence[i][0].toUpperCase() + sentence[i].slice(1).toLowerCase());
+}
+
+console.log(arr.join(" "));
