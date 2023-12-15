f6ac151fbda28069d8f000083f2a1dbef2887e07
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-12-15 20:31:20 +0300

    added tasks

--- 041.Typescript_4/dist/app7.js
+++ 041.Typescript_4/dist/app7.js
@@ -1,4 +1,19 @@
 //Создайте класс TodoList,который содержит свойство tasks(списокзадач)и методы addTask() (добавление задачи), removeTask() (удаление задачи) и
 //getTasks() (получение списка задач). Создайте экземпляр класса TodoList и выполните несколько операций.
 class TodoList {
+    tasks = {};
+    addTask(it) {
+        this.tasks = it;
+    }
+    removeTask(it) {
+        delete this.tasks[it];
+    }
+    getTasks() {
+        return this.tasks;
+    }
 }
+const todoList = new TodoList();
+todoList.addTask("work");
+todoList.addTask("sleep");
+todoList.removeTask("work");
+console.log(todoList.getTasks());
--- 041.Typescript_4/src/app7.ts
+++ 041.Typescript_4/src/app7.ts
@@ -1,6 +1,24 @@
 //Создайте класс TodoList,который содержит свойство tasks(списокзадач)и методы addTask() (добавление задачи), removeTask() (удаление задачи) и
 //getTasks() (получение списка задач). Создайте экземпляр класса TodoList и выполните несколько операций.
 
-class TodoList{
-    
+class TodoList {
+  tasks = {};
+
+  addTask(it:string) {
+    this.tasks = it;
+  }
+
+  removeTask(it:string) {
+    delete this.tasks[it];
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
 }
+
+const todoList = new TodoList();
+todoList.addTask("work");
+todoList.addTask("sleep");
+todoList.removeTask("work");
+console.log(todoList.getTasks());
