bcd5e84ca30892b5cc0dddca2be877673a5f2c46
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-12-18 21:18:12 +0300

    added tasks

---
+++ 041.Typescript_4/src/app8.ts
@@ -0,0 +1,12 @@
+//Создайте класс CircleR, который содержит свойство radius(радиус круга)и метод getArea() (вычисление площади круга). Создайте экземпляр класса
+//CircleR и выведите площадь окружности. Площадь круга: π * r ^ 2
+
+class CircleR {
+  radius: number = 15;
+  getArea(r: number): number {
+    return 3.14 * r ** 2;
+  }
+}
+
+const circleR = new CircleR();
+console.log(circleR.getArea(circleR.radius));
