4a7ed5ee933c68a0403e6af16edd0f7a3aea330c
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-12-27 20:27:57 +0300

    added task

---
+++ 042.Typescript_5 OOP/dist/app8.js
@@ -0,0 +1,15 @@
+//Создайте класс PasswordGenerator,который будет иметь метод generatePassword, позволяющий генерировать случайные безопасные пароли заданной длины.
+//Метод generatePassword принимает в качестве параметра длину пароля. ИспользоватьGenerics
+class PasswordGenerator {
+    generatePassword(pwd) {
+        if (typeof pwd == "number") {
+            let str = [];
+            for (let i = 0; i < pwd; i++) {
+                str.push(Math.floor(Math.random() * 10));
+            }
+            return str;
+        }
+    }
+}
+const passwordGenerator = new PasswordGenerator();
+console.log(passwordGenerator.generatePassword(5));
---
+++ 042.Typescript_5 OOP/src/app8.ts
@@ -0,0 +1,17 @@
+//Создайте класс PasswordGenerator,который будет иметь метод generatePassword, позволяющий генерировать случайные безопасные пароли заданной длины.
+//Метод generatePassword принимает в качестве параметра длину пароля. ИспользоватьGenerics
+
+class PasswordGenerator {
+  generatePassword<T>(pwd: T) {
+    if (typeof pwd == "number") {
+      let str: number[] = [];
+      for (let i = 0; i < pwd; i++) {
+        str.push(Math.floor(Math.random() * 10));
+      }
+      return str;
+    }
+  }
+}
+
+const passwordGenerator = new PasswordGenerator();
+console.log(passwordGenerator.generatePassword<number>(5));
