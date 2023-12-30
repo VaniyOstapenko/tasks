c521fcc6d028fca82e78f0d51df2ac526b593cf6
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-12-30 12:43:19 +0300

    added task

---
+++ 044.UnitTestTs/app.test.ts
@@ -0,0 +1,71 @@
+import {
+  isPalindrome,
+  calculateFactorial,
+  capitalizeString,
+  StringArray,
+} from "./app";
+
+describe("test isPalindrom function", () => {
+  test("возвращает успех", () => {
+    const result = isPalindrome("anna");
+    expect(result).toBeTruthy();
+  });
+
+  test("Возвращает ошибку empty", () => {
+    const result = isPalindrome("");
+    expect(result).toBe("empty");
+  });
+
+  test("ошибка типа данных", () => {
+    const result = isPalindrome(6);
+    expect(result).toBe("word must be a string");
+  });
+});
+
+describe("test calculateFactorial function", () => {
+  test("to be success", () => {
+    const result = calculateFactorial(3);
+    expect(result).toBe(6);
+  });
+
+  test("Возвращает ошибку empty", () => {
+    const result = calculateFactorial("");
+    expect(result).toBe("empty");
+  });
+
+  test("ошибка типа данных", () => {
+    const result = calculateFactorial("6");
+    expect(result).toBe("n must be a number");
+  });
+});
+
+describe("test  capitalizeString function", () => {
+  test("to be success", () => {
+    const result = capitalizeString("hello vania");
+    expect(result).toBe("HelloVania");
+  });
+
+  test("to be empty", () => {
+    const result = capitalizeString("");
+    expect(result).toBe("empty");
+  });
+
+  test("to be typeof", () => {
+    const result = capitalizeString(123);
+    expect(result).toBe("str must be a string");
+  });
+});
+
+describe("test StringArray class", () => {
+  test("вoзвращает success", () => {
+    const stringArray = new StringArray();
+    const result = stringArray.getLongestWord();
+    expect(result).toBe("vania");
+  });
+
+  test("вoзвращает success", () => {
+    const stringArray = new StringArray();
+    const result = stringArray.getUniqueWords();
+    expect(result).toEqual(["helo", "vania", "hi"]);
+  });
+});
---
+++ 044.UnitTestTs/app.ts
@@ -0,0 +1,82 @@
+//1. Напишите функцию isPalindrome(word: string):boolean, которая принимает слово и возвращает true, если оно является палиндромом
+//(читается одинаково слева направо и справа налево),и false в противном случае.
+
+function isPalindrome(word: string | number): boolean {
+  try {
+    if (!word) throw new Error("empty");
+    if (typeof word == "number") throw new Error("word must be a string");
+    return word == word.split("").reverse().join("") ? true : false;
+  } catch (error: any) {
+    return error.message;
+  }
+}
+
+//2. Напишите функцию calculateFactorial(n: number): number, которая принимает
+//число nи возвращает его факториал.
+
+function calculateFactorial(n: any): number {
+  try {
+    if (!n) throw new Error("empty");
+    if (typeof n == "string") throw new Error("n must be a number");
+    if (n <= 1) return 1;
+    return n * calculateFactorial(n - 1);
+  } catch (error: any) {
+    return error.message;
+  }
+}
+
+//3. Напишите функцию capitalizeString(str: string): string, которая принимает
+//строку и возвращает новую строку,в которой каждое слово начинается с
+//заглавной буквы.
+
+function capitalizeString(str: any): string | number {
+  try {
+    if (!str) throw new Error("empty");
+    if (typeof str == "number") throw new Error("str must be a string");
+    let newStr = str.split(" ");
+    let res = "";
+    for (let i = 0; i < newStr.length; i++) {
+      res += newStr[i][0].toUpperCase() + newStr[i].slice(1);
+    }
+    return res;
+  } catch (error: any) {
+    return error.message;
+  }
+}
+
+//8. Создайте класс StringArray, который имеет свойство array (массив строк) и
+//методы: getLongestWord(): string -возвращает самое длинное слово из массива.
+//getUniqueWords(): string[] -возвращает массив уникальных слов из массива.
+
+class StringArray {
+  array: string[] = ["helo", "vania", "hi", "hi"];
+  getLongestWord(): string {
+    try {
+      let res: string = this.array[0];
+      for (let i = 0; i < this.array.length; i++) {
+        if (res.length < this.array[i].length) {
+          res = this.array[i];
+        }
+      }
+      return res;
+    } catch (error: any) {
+      return error.message;
+    }
+  }
+
+  getUniqueWords(): string[] {
+    try {
+      let newArr: string[] = [];
+      for (let i = 0; i < this.array.length; i++) {
+        if (!newArr.includes(this.array[i])) {
+          newArr.push(this.array[i]);
+        }
+      }
+      return newArr;
+    } catch (error: any) {
+      return error.message;
+    }
+  }
+}
+
+export { isPalindrome, calculateFactorial, capitalizeString, StringArray };
